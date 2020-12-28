f845cb48501a604f54cc6c12e269aa85508e91df
heqidu <787491096@qq.com>
2020-12-28 17:04:35 +0800

    添加粘贴板复制操作

--- app/src/main/java/com/ytempest/wanandroid/activity/main/project/content/list/ContentListAdapter.java
+++ app/src/main/java/com/ytempest/wanandroid/activity/main/project/content/list/ContentListAdapter.java
@@ -1,5 +1,11 @@
 package com.ytempest.wanandroid.activity.main.project.content.list;
 
+import android.content.ClipData;
+import android.content.ClipDescription;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,6 +14,8 @@ import android.widget.ImageView;
 
 import com.ytempest.tool.adapter.CoreRecyclerAdapter;
 import com.ytempest.tool.adapter.CoreViewHolder;
+import com.ytempest.tool.helper.ActivityLauncher;
+import com.ytempest.tool.util.ToastUtils;
 import com.ytempest.wanandroid.R;
 import com.ytempest.wanandroid.activity.article.ArticleDetailActivity;
 import com.ytempest.wanandroid.activity.main.project.content.ProjectContentPresenter;
@@ -28,6 +36,24 @@ public class ContentListAdapter extends CoreRecyclerAdapter<ProjectContentBean.D
         public void onClick(View v) {
             ProjectContentBean.DatasBean data = (ProjectContentBean.DatasBean) v.getTag();
             mPresenter.addProjectArticleCollect(data);
+        }
+    };
+    private final View.OnClickListener mVcsClickListener = v -> {
+        Uri url = Uri.parse((String) v.getTag());
+        ActivityLauncher.startActivity(v.getContext(), new Intent(Intent.ACTION_VIEW, url));
+    };
+    private final View.OnLongClickListener mVcsLongClickListener = new View.OnLongClickListener() {
+        private ClipboardManager mManager;
+
+        @Override
+        public boolean onLongClick(View v) {
+            if (mManager == null) {
+                mManager = (ClipboardManager) v.getContext().getSystemService(Context.CLIPBOARD_SERVICE);
+            }
+            String projectUrl = (String) v.getTag();
+            mManager.setPrimaryClip(ClipData.newPlainText(ClipDescription.MIMETYPE_TEXT_PLAIN, projectUrl.trim()));
+            ToastUtils.show(v.getContext(), R.string.copy_to_clipboard);
+            return true;
         }
     };
 
@@ -54,6 +80,10 @@ public class ContentListAdapter extends CoreRecyclerAdapter<ProjectContentBean.D
         holder.setText(R.id.tv_item_project_content_title, data.getTitle());
         holder.setText(R.id.tv_item_project_content_desc, data.getDesc());
         holder.setText(R.id.tv_item_project_content_time, DateFormat.format(data.getPublishTime()));
+        View vcsView = holder.getViewById(R.id.tv_item_project_content_vcs_url);
+        vcsView.setTag(data.getProjectLink());
+        vcsView.setOnClickListener(mVcsClickListener);
+        vcsView.setOnLongClickListener(mVcsLongClickListener);
 
         // TODO : ProjectContentBean.DatasBean#isCollect()这个方法未能获取正确的收藏状态，需要等服务器支持后再做处理
         View collectView = holder.getViewById(R.id.tv_item_project_content_collect);
