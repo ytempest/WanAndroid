f75fe56055ed9ff534c41d5019123551ea064b31
ytempest <787491096@qq.com>
2021-02-02 15:28:39 +0800

    更新加载动画逻辑

--- app/src/main/java/com/ytempest/wanandroid/activity/main/knowledge/KnowledgeFrag.java
+++ app/src/main/java/com/ytempest/wanandroid/activity/main/knowledge/KnowledgeFrag.java
@@ -8,12 +8,10 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
 import com.ytempest.layoutinjector.annotation.InjectLayout;
-import com.ytempest.tool.util.LogUtils;
 import com.ytempest.wanandroid.R;
 import com.ytempest.wanandroid.base.fragment.LoaderFrag;
 import com.ytempest.wanandroid.base.load.ViewType;
 import com.ytempest.wanandroid.http.bean.KnowledgeArchitectureBean;
-import com.ytempest.wanandroid.utils.JSON;
 
 import java.util.Collections;
 import java.util.List;
@@ -58,9 +56,6 @@ public class KnowledgeFrag extends LoaderFrag<KnowledgePresenter> implements IKn
         getLoader().hideAll();
         Collections.sort(list, (o1, o2) -> Integer.compare(o1.getOrder(), o2.getOrder()));
         mAdapter.display(list);
-        for (int i = 0; i < list.size(); i++) {
-            LogUtils.d(TAG, "onKnowledgeArchitectureReceived: bean=" + JSON.toJson(list.get(i)));
-        }
     }
 
     @Override
--- app/src/main/java/com/ytempest/wanandroid/base/fragment/LoaderFrag.java
+++ app/src/main/java/com/ytempest/wanandroid/base/fragment/LoaderFrag.java
@@ -1,19 +1,12 @@
 package com.ytempest.wanandroid.base.fragment;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.ytempest.tool.util.NetUtils;
-import com.ytempest.wanandroid.R;
-import com.ytempest.wanandroid.base.fragment.MvpFragment;
-import com.ytempest.wanandroid.base.load.AbsViewType;
 import com.ytempest.wanandroid.base.load.Loader;
-import com.ytempest.wanandroid.base.load.ViewType;
 import com.ytempest.wanandroid.base.presenter.IPresenter;
 
 /**
@@ -21,6 +14,8 @@ import com.ytempest.wanandroid.base.presenter.IPresenter;
  * @since 2020/12/28
  */
 public class LoaderFrag<Presenter extends IPresenter> extends MvpFragment<Presenter> {
+
+    private static final String TAG = LoaderFrag.class.getSimpleName();
 
     private Loader mLoader;
 
@@ -33,22 +28,7 @@ public class LoaderFrag<Presenter extends IPresenter> extends MvpFragment<Presen
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         mLoader = new Loader((ViewGroup) view);
-        mLoader.setViewListener(ViewType.ERR, new AbsViewType.OnViewActionListener() {
-            @Override
-            public void onViewCreated(View view) {
-                super.onViewCreated(view);
-                view.findViewById(R.id.tv_net_error_retry)
-                        .setOnClickListener(v -> {
-                            if (NetUtils.isNetAvailable(getContext())) {
-                                mLoader.showView(ViewType.LOAD);
-                                onReloadClick();
-                            }
-                        });
-                view.findViewById(R.id.tv_net_error_setting_net)
-                        .setOnClickListener(v -> startActivity(new Intent(Settings.ACTION_SETTINGS)));
-            }
-        });
-        mLoader.setViewListener(ViewType.LOAD, new AbsViewType.OnViewActionListener());
+        mLoader.setReloadCall(aVoid -> onReloadClick());
     }
 
     protected void onReloadClick() {
